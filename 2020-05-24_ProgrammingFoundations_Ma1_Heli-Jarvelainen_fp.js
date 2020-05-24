3d2ca1a787bb8e3514c1eddfb37509b82207ec68
heliuxdesign <61623649+heliuxdesign@users.noreply.github.com>
2020-05-24 11:20:19 +0200

    Create 2020-05-24_ProgrammingFoundations_Ma1_Heli-Jarvelainen_fp.js

---
+++ 2020-05-24_ProgrammingFoundations_Ma1_Heli-Jarvelainen_fp.js
@@ -0,0 +1,30 @@
+/* question 1 */
+var firstVar;
+
+/* question 2 */
+var name = "Heli";
+
+/* question 3 */
+var number = 5;
+
+/* question 4 */
+var devision = 20 / 5;
+
+/* question 5 */
+var animal = "frog";
+console.log(typeof animal);
+
+/* question 6 */
+var orderHasShipped = true;
+
+/* question 7 */
+if (orderHasShipped) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+
+/* question 8 */
+for (var i = 0; i < 10; i++) {
+	console.log(i);
+}
